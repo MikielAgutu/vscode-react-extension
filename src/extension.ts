6f06fe9b3425157c0230521ac34398f16880421d
MikielAgutu <mikiel.agutu@red-gate.com>
2019-07-11 10:58:24 +0100

    Introduce React to application

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,9 @@ const webViewPanelTitle = "Custom extension";
 const webViewPanelId = "customExtension";
 
 function startCommandHandler(context: vscode.ExtensionContext) : void {
-  const showOptions = {};
+  const showOptions = {
+    enableScripts: true
+  };
   const panel = vscode.window.createWebviewPanel(
     webViewPanelId,
     webViewPanelTitle,
@@ -32,7 +34,42 @@ function getHtmlForWebview() : string {
   </head>
   <body>
       <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-  </body>
+      <br />
+      <strong>React application running in VsCode:</strong><br />
+      <div id="react-app-container"></div>
+
+      <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+      <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+    
+      <!-- Load our React component. -->
+      <script>
+        'use strict';
+
+        const e = React.createElement;
+
+        class LikeButton extends React.Component {
+          constructor(props) {
+            super(props);
+            this.state = { liked: false };
+          }
+
+          render() {
+            if (this.state.liked) {
+              return 'You liked this.';
+            }
+
+            return e(
+              'button',
+              { onClick: () => this.setState({ liked: true }) },
+              'Like'
+            );
+          }
+        }
+
+        const domContainer = document.querySelector('#react-app-container');
+        ReactDOM.render(e(LikeButton), domContainer);
+      </script>
+    </body>
   </html>`;
 }
 
