9b188e69484acaf22eda94d0faa359b447dda246
MikielAgutu <mikiel.agutu@red-gate.com>
2019-07-11 15:12:31 +0100

    Use Webpack dev server

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return <div>Hello from React!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
