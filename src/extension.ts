1f0857ee1307f799535bef6ee52c40aca5c54148
MikielAgutu <mikiel.agutu@red-gate.com>
2019-07-11 10:26:52 +0100

    init

---
+++ src/extension.ts
@@ -0,0 +1,15 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+  console.log('Congratulations, your extension "testextension" is now active!');
+
+
+  let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World!');
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
