a286c3e241eb3db7271363fd5a602fc1fc424e5e
MikielAgutu <mikiel.agutu@red-gate.com>
2019-07-12 09:51:45 +0100

    Move meme into react component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import InteractorFactory from './Interaction/InteractorFactory';
 
-const Interactor = InteractorFactory.create(); 
+const Interactor = InteractorFactory.create();
 
 const Index = () => {
   return <>
-  <button onClick={() => {Interactor.showInformationMessage("Emojis are in vogue at the moment 🐛")}}>Click me</button>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" /><br />
+    <button onClick={() => { Interactor.showInformationMessage("Emojis are in vogue at the moment 🐛") }}>Click me</button>
     <div className="red">Hello from React!</div>
   </>
 };
